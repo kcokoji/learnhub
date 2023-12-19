fcfbd59c2f48546228dcfa95dc8854a7cbdcb545
okoji Kelechi <kelechiokoji52@gmail.com>
2023-12-19 12:14:22 +0100

    Fixed chapter transaction bug

--- app/api/courses/[courseId]/published/route.js
+++ app/api/courses/[courseId]/published/route.js
@@ -20,6 +20,18 @@ export async function PATCH(res, { params }) {
   }
 
   try {
+    const transaction = await prismadb.transaction.findFirst({
+      where: {
+        courseId,
+      },
+    });
+    if (transaction) {
+      // If a transaction exists, throw an error
+      throw new Error(
+        "Course has been purchased by a student and cannot be deleted."
+      );
+    }
+
     const updatedPublish = await prismadb.course.update({
       where: {
         id: courseId,
